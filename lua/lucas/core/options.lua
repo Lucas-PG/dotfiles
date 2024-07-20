f5789b40911cabfe120459ead4c1dac3e8292de7
Lucas Gasperin <luscagape@gmail.com>
2024-07-20 12:05:29 -0300

    chore(options): set conceallevel to 2
    For obsidian.nvim to work

--- lua/lucas/core/options.lua
+++ lua/lucas/core/options.lua
@@ -1,5 +1,6 @@
-vim.cmd "let g:netrw_liststyle = 3"
+vim.cmd("let g:netrw_liststyle = 3")
 local opt = vim.opt
+vim.o.conceallevel = 2
 
 opt.relativenumber = true
 opt.number = true
@@ -14,7 +15,7 @@ opt.termguicolors = true
 opt.background = "dark"
 opt.signcolumn = "yes"
 opt.backspace = "indent,eol,start"
-opt.clipboard:append "unnamedplus"
+opt.clipboard:append("unnamedplus")
 opt.splitright = true
 opt.splitbelow = true
 opt.autoread = true
@@ -24,3 +25,4 @@ vim.g.blamer_enabled = true
 vim.g.blamer_delay = 300
 vim.g.blamer_show_in_visual_modes = 0
 vim.g.blamer_show_in_insert_modes = 0
+
