8fd351a9d03ca331286523b2ef1548f8b48a345d
Lucas Gasperin <luscagape@gmail.com>
2024-07-28 10:04:55 -0300

    add code snap

---
+++ lua/lucas/plugins/codesnap.lua
@@ -0,0 +1,13 @@
+return {
+	"mistricky/codesnap.nvim",
+	build = "make",
+	opts = {
+		save_path = "~/Pictures/ScreenShots/",
+		bg_theme = "grape",
+		has_breadcrumbs = false,
+		has_line_number = true,
+		watermark = "",
+		mac_window_bar = false,
+		-- show_workspace = true,
+	},
+}
