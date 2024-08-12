e4164d85674d06c644d69af69446a982b3d860ec
Lucas-PG <luscagape@gmail.com>
2024-08-12 14:53:17 -0300

    chore(keymaps): remove nvchad keymaps
    from the time I used nvchad

--- lua/lucas/core/keymaps.lua
+++ lua/lucas/core/keymaps.lua
@@ -75,28 +75,6 @@ map("n", "<leader>fd", "<cmd>Telescope dir live_grep", { desc = "Telescope insid
 -- terminal
 map("t", "<C-q>", "<C-\\><C-N>", { desc = "Terminal escape terminal mode" })
 
--- new terminals
--- map("n", "<leader>h", function()
---   require("nvchad.term").new { pos = "sp" }
--- end, { desc = "terminal new horizontal term" })
-
--- map("n", "<leader>v", function()
---   require("nvchad.term").new { pos = "vsp" }
--- end, { desc = "terminal new vertical window" })
-
--- -- toggleable
--- map({ "n", "t" }, "<A-v>", function()
---   require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
--- end, { desc = "terminal toggleable vertical term" })
-
--- map({ "n", "t" }, "<A-h>", function()
---   require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
--- end, { desc = "terminal new horizontal term" })
-
--- map({ "n", "t" }, "<A-i>", function()
---   require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
--- end, { desc = "terminal toggle floating term" })
-
 -- whichkey
 map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
 
