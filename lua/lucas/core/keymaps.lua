659d0fdbdb553bcdb5789118f2d5a161c60ba4a3
Lucas-PG <luscagape@gmail.com>
2024-08-13 15:15:59 -0300

    remove nvim tree keymaps
    Never used them, so they piss me off

--- lua/lucas/core/keymaps.lua
+++ lua/lucas/core/keymaps.lua
@@ -3,9 +3,6 @@ local map = vim.keymap.set
 
 -- NvimTree
 map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
--- map("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
--- map("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
--- map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
 
 map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
 map("i", "<C-e>", "<End>", { desc = "move end of line" })
