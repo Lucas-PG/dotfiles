86ecb83071963df54dfcd901b4fcc7445115a749
Lucas Gasperin <luscagape@gmail.com>
2024-10-11 11:51:27 -0300

    make every theme have a transparent bg

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
 require("lucas.core")
 require("lucas.lazy")
 require("colorscheme")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
