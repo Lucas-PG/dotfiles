3dd72c597caecea3809ea3669c899edcbac7fab7
Lucas Gasperin <luscagape@gmail.com>
2024-10-12 15:06:58 -0300

    config for octo nvim

--- lua/lucas/plugins/init.lua
+++ lua/lucas/plugins/init.lua
@@ -40,18 +40,18 @@ return {
 	"eandrju/cellular-automaton.nvim",
 	"sudormrfbin/cheatsheet.nvim",
 	"princejoogie/dir-telescope.nvim",
-	-- {
-	-- 	"pwntester/octo.nvim",
-	-- 	requires = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"nvim-telescope/telescope.nvim",
-	-- 		-- OR 'ibhagwan/fzf-lua',
-	-- 		"nvim-tree/nvim-web-devicons",
-	-- 	},
-	-- 	config = function()
-	-- 		require("octo").setup()
-	-- 	end,
-	-- },
+	{
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			-- OR 'ibhagwan/fzf-lua',
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup()
+		end,
+	},
 	{
 		"HakonHarnes/img-clip.nvim",
 		event = "VeryLazy",
