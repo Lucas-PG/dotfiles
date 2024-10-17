4b07c8f33ab182a332cbc7701bab318535533ab4
Lucas Gasperin <luscagape@gmail.com>
2024-10-17 08:24:15 -0300

    add commands to generate logs commented out

--- init.lua
+++ init.lua
@@ -5,3 +5,8 @@ require("colorscheme")
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+
+-- vim.loader.enable() -- Neovim native lazy loader
+-- vim.cmd("profile start nvim_profile.log")
+-- vim.cmd("profile func *")
+-- vim.cmd("profile file *")
