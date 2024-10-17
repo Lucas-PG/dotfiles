4aad217abf0a206f02e03c3fb81b2f6e8550dbd3
Lucas Gasperin <luscagape@gmail.com>
2024-10-17 08:15:45 -0300

    configure nvim-cmp

--- lua/lucas/plugins/nvim-cmp.lua
+++ lua/lucas/plugins/nvim-cmp.lua
@@ -1,63 +1,160 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-path", -- source for file system paths
-    {
-      "L3MON4D3/LuaSnip",
-      -- follow latest release.
-      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-      -- install jsregexp (optional!).
-      build = "make install_jsregexp",
-    },
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
-    "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
-  },
-  config = function()
-    local cmp = require("cmp")
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		-- "onsails/lspkind.nvim",
+		{
+			"garymjr/nvim-snippets",
+			opts = {
+				friendly_snippets = true,
+			},
+			dependencies = { "rafamadriz/friendly-snippets" },
+		},
+	},
 
-    local luasnip = require("luasnip")
+	config = function()
+		local cmp = require("cmp")
+		local max_items = 5
+		local kind_icons = {
+			Array = "",
+			Boolean = "󰨙",
+			Class = "󰠱",
+			Color = "󰏘",
+			Control = "",
+			Collapsed = "",
+			Constant = "󰏿",
+			Constructor = "",
+			Copilot = "",
+			Enum = "⌘",
+			EnumMember = "⌘",
+			Event = "",
+			Field = "",
+			File = "",
+			Folder = "",
+			Function = "󰊕",
+			Interface = "",
+			Keyword = "",
+			Method = "󰊕",
+			Module = "󰆧",
+			Package = " ",
+			Namespace = "󰦮",
+			Null = "",
+			Number = "󰎠",
+			Operator = "󰆕",
+			Object = "󰘦",
+			Property = "",
+			Reference = "󰈇",
+			-- Snippet = "",
+			Snippet = "",
+			Struct = "󰆼",
+			Text = "",
+			String = "",
+			TypeParameter = "󰅲",
+			Value = "󰎠",
+			Unit = "",
+			Variable = "𝑣",
+		}
+		cmp.setup({
+			window = {
+                -- stylua: ignore start
 
-    local lspkind = require("lspkind")
+                -- { "╭", "Comment" },
+                completion = {
+                    border = { { "󱐋", "WarningMsg" }, { "─", "Comment" }, { "╮", "Comment" }, { "│", "Comment" }, { "╯", "Comment" }, { "─", "Comment" }, { "╰", "Comment" }, { "│", "Comment" }, },
+                    scrollbar = false,
+                },
+                documentation = {
+                    border = { { "i", "DiagnosticHint" }, { "─", "Comment" }, { "╮", "Comment" }, { "│", "Comment" }, { "╯", "Comment" }, { "─", "Comment" }, { "╰", "Comment" }, { "│", "Comment" }, },
+                    scrollbar = false,
+                },
+				-- stylua: ignore end
+			},
 
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require("luasnip.loaders.from_vscode").lazy_load()
+			completion = {
+				completeopt = "menu,menuone,preview,noinsert",
+			},
 
-    cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,preview,noselect",
-      },
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      }),
-      -- sources for autocompletion
-      sources = cmp.config.sources({
-        { name = "nvim_lsp"},
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
-      }),
+            -- stylua: ignore start
+            mapping = cmp.mapping.preset.insert({
+                ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<cr>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true, }),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            }),
+			-- stylua: ignore end
 
-      -- configure lspkind for vs-code like pictograms in completion menu
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-        }),
-      },
-    })
-  end,
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "snippets" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+
+			-- lsp-kind icons & custom cmp menu icons
+			-- if you want one less dependency , visit : https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#basic-customisations
+			formatting = {
+				-- format = require("lspkind").cmp_format({ mode = "symbol_text", }),
+				format = function(_, vim_item)
+					-- vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
+					vim_item.kind = string.format(" %s", kind_icons[vim_item.kind])
+					-- Source
+					-- vim_item.menu = ({
+					--     buffer = "[Buffer]",
+					--     nvim_lsp = "[LSP]",
+					--     luasnip = "[LuaSnip]",
+					--     nvim_lua = "[Lua]",
+					--     latex_symbols = "[LaTeX]",
+					-- })[entry.source.name]
+					return vim_item
+				end,
+			},
+
+			experimental = {
+				ghost_text = true,
+			},
+
+			view = {
+				entries = {
+					name = "custom",
+					selection_order = "top_down",
+					follow_cursor = false,
+				},
+				docs = {
+					auto_open = true,
+				},
+			},
+		})
+
+		-- insert braces after selecting function or method item
+		cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+	end,
+
+	keys = {
+		{
+			"<Tab>",
+			function()
+				return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
+			end,
+			expr = true,
+			silent = true,
+			mode = { "i", "s" },
+		},
+		{
+			"<S-Tab>",
+			function()
+				return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<Tab>"
+			end,
+			expr = true,
+			silent = true,
+			mode = { "i", "s" },
+		},
+	},
 }
