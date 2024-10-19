aeffc6797b437f45476fa6bd7f395e77e87d8513
Lucas Gasperin <luscagape@gmail.com>
2024-10-19 13:27:47 -0300

    nvim: tokyonight lualine

--- nvim/lua/lucas/plugins/lualine.lua
+++ nvim/lua/lucas/plugins/lualine.lua
@@ -1,31 +1,31 @@
 return {
-	-- "nvim-lualine/lualine.nvim",
-	-- dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- config = function()
-	-- 	local lualine = require("lualine")
-	-- 	local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-	-- 	-- configure lualine with modified theme
-	-- 	lualine.setup({
-	-- 		options = {
-	-- 			-- theme = "tokyonight",
-	-- 			-- theme = "nightfly",
-	-- 			-- theme = "iceberg_dark",
-	-- 			-- theme = "vscode",
-	-- 			-- theme = "nord",
-	-- 			theme = "solarized_dark",
-	-- 		},
-	-- 		sections = {
-	-- 			lualine_x = {
-	-- 				{
-	-- 					lazy_status.updates,
-	-- 					cond = lazy_status.has_updates,
-	-- 					-- color = { fg = "#ff9e64" },
-	-- 				},
-	-- 				{ "encoding" },
-	-- 				{ "fileformat" },
-	-- 				{ "filetype" },
-	-- 			},
-	-- 		},
-	-- 	})
-	-- end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
+		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+		-- configure lualine with modified theme
+		lualine.setup({
+			options = {
+				theme = "tokyonight",
+				-- theme = "nightfly",
+				-- theme = "iceberg_dark",
+				-- theme = "vscode",
+				-- theme = "nord",
+				-- theme = "solarized_dark",
+			},
+			sections = {
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						-- color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
