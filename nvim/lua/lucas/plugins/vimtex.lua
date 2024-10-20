d5efd95cd487e208ed8c33e652a6058ef382d404
Lucas Gasperin <luscagape@gmail.com>
2024-10-20 07:07:34 -0300

    configure vimtex

--- nvim/lua/lucas/plugins/vimtex.lua
+++ nvim/lua/lucas/plugins/vimtex.lua
@@ -2,6 +2,11 @@ return {
 	"lervag/vimtex",
 	lazy = false,
 	init = function()
-		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_view_method = "skim"
+		vim.g.vimtex_view_skim_sync = 1
+		vim.g.vimtex_view_skim_activate = 1
+		vim.g.tex_flavor = "latex"
 	end,
+
+	ft = { "tex", "latex" },
 }
