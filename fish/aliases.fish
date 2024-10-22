794be12a51e4095bbda74276afb5922d70314ff0
Lucas-PG <luscagape@gmail.com>
2024-10-22 15:00:02 -0300

    fish: update aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,12 +1,22 @@
-alias c="clear"
-alias l="ls -l"
-alias ll="ls -la"
-alias la="ls -a"
-alias lg="lazygit"
-alias g="git"
-alias gx="git reset --hard HEAD && git clean -fd"
-alias t="tmux"
-alias vim="nvim"
-alias cd="z"
-alias ls="eza --icons=always"
+alias c "clear"
+alias l "ls -l"
+alias ll "ls -la"
+alias la "ls -a"
+alias lg "lazygit"
+alias g "git"
+alias gx "git reset --hard HEAD && git clean -fd"
+alias t "tmux"
+alias vim "nvim"
+alias cd "z"
+
+
+# In case I ever use Windows 
+switch (uname)
+  case Darwin
+    alias ls "eza --icons=always"
+  case Linux
+    alias run-boot "sudo bash -x /mnt/suporte/scripts/boot.sh"
+  case '*'
+end
+
 
