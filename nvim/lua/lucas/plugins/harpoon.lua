a1955861be6fd7b853445550d59f897a9df79a16
Lucas-PG <luscagape@gmail.com>
2024-10-22 16:17:49 -0300

    nvim: add description to harpoon maps

--- nvim/lua/lucas/plugins/harpoon.lua
+++ nvim/lua/lucas/plugins/harpoon.lua
@@ -6,36 +6,42 @@ return {
 			function()
 				require("harpoon.mark").add_file()
 			end,
+			{ desc = "add file to harpoon" },
 		},
 		{
-			"<C-e>",
+			"<leader>e",
 			function()
 				require("harpoon.ui").toggle_quick_menu()
 			end,
+			{ desc = "harpoon toggle quick menu" },
 		},
 		{
 			"<leader>1",
 			function()
 				require("harpoon.ui").nav_file(1)
 			end,
+			{ desc = "harpoon go to file 1" },
 		},
 		{
 			"<leader>2",
 			function()
 				require("harpoon.ui").nav_file(2)
 			end,
+			{ desc = "harpoon go to file 2" },
 		},
 		{
 			"<leader>3",
 			function()
 				require("harpoon.ui").nav_file(3)
 			end,
+			{ desc = "harpoon go to file 3" },
 		},
 		{
 			"<leader>4",
 			function()
 				require("harpoon.ui").nav_file(4)
 			end,
+			{ desc = "harpoon go to file 4" },
 		},
 	},
 }
