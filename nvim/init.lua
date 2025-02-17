6c6581da4a94fb01bdd313c5569066b7b4992611
Lucas-PG <luscagape@gmail.com>
2025-02-17 08:09:50 -0300

    chore: entende .tf files como terraform

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,13 @@ require("lucas.core")
 require("lucas.lazy")
 require("colorscheme")
 
+vim.cmd([[
+  augroup terraform_ft
+    autocmd!
+    autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform
+  augroup END
+]])
+
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
