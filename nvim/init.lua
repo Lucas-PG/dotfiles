d1602c30f58cff074837f2e6298911f4821ecc66
Lucas-PG <luscagape@gmail.com>
2026-03-02 13:23:00 -0300

    remove duplicates, 99 and update pack_clean

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,13 +14,25 @@ vim.g.mapleader = " "
 vim.o.clipboard = "unnamedplus"
 vim.o.timeoutlen = 500
 vim.o.completeopt = "menu,menuone,noselect"
+vim.o.autoread = true
+vim.o.colorcolumn = "80"
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", ";", ":")
 
+local autoread_group = vim.api.nvim_create_augroup("autoread_checktime", { clear = true })
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI", "TermLeave", "WinEnter" }, {
+	group = autoread_group,
+	callback = function()
+		if vim.fn.mode() ~= "c" then
+			vim.cmd("checktime")
+		end
+	end,
+})
+
 vim.pack.add({
 	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/ThePrimeagen/99" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
 	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
@@ -28,7 +40,7 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/williamboman/mason-lspconfig.nvim" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 	{ src = "https://github.com/christoomey/vim-tmux-navigator" },
 	{ src = "https://github.com/windwp/nvim-autopairs" },
 	{ src = "https://github.com/rmagatti/auto-session" },
@@ -36,12 +48,15 @@ vim.pack.add({
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 	{ src = "https://github.com/kdheepak/lazygit.nvim" },
 	{ src = "https://github.com/folke/tokyonight.nvim" },
+	{ src = "https://github.com/hat0uma/csvview.nvim" },
+	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
 	{ src = "https://github.com/nvim-mini/mini.indentscope" },
 	{ src = "https://github.com/tpope/vim-fugitive" },
 	{ src = "https://github.com/tpope/vim-rhubarb" },
 	{ src = "https://github.com/mfussenegger/nvim-jdtls" },
 	{ src = "https://github.com/iamcco/markdown-preview.nvim" },
 	{ src = "https://github.com/rose-pine/neovim" },
+
 
 	{ src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
 	{ src = "https://github.com/hrsh7th/cmp-buffer" },
@@ -53,53 +68,6 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/vim-vscode-snippets" },
 	{ src = "https://github.com/saadparwaiz1/cmp_luasnip" },
 	{ src = "https://github.com/windwp/nvim-ts-autotag" },
-})
-
-local function clean_removed_packs()
-	-- Remove plugins that are still on disk but no longer declared in vim.pack.add().
-	if not (vim.pack and vim.pack.get and vim.pack.del) then
-		return
-	end
-
-	local stale_specs = vim.iter(vim.pack.get())
-		:filter(function(plugin)
-			return not plugin.active
-		end)
-		:map(function(plugin)
-			local spec = plugin.spec or {}
-			if type(spec.src) == "string" and spec.src ~= "" then
-				return { src = spec.src }
-			end
-			return nil
-		end)
-		:filter(function(spec)
-			return spec ~= nil
-		end)
-		:totable()
-
-	if #stale_specs == 0 then
-		return
-	end
-
-	local hard_errors = {}
-	for _, spec in ipairs(stale_specs) do
-		local ok, err = pcall(vim.pack.del, { spec })
-		if not ok then
-			local msg = tostring(err)
-			if not msg:match("is not installed") then
-				table.insert(hard_errors, msg)
-			end
-		end
-	end
-
-	if #hard_errors > 0 then
-		vim.notify("vim.pack cleanup failed: " .. hard_errors[1], vim.log.levels.WARN)
-	end
-end
-
-vim.api.nvim_create_autocmd("VimEnter", {
-	once = true,
-	callback = clean_removed_packs,
 })
 
 -- require("vague").setup({
@@ -165,16 +133,6 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 	end,
 })
 
-local ok_99, _99 = pcall(require, "99")
-if ok_99 then
-	_99.setup({
-		provider = _99.Providers.ClaudeCodeProvider,
-		logger = {
-			level = _99.DEBUG,
-		},
-	})
-end
-
 require("oil").setup({
 	lsp_file_methods = {
 		enabled = true,
@@ -211,33 +169,57 @@ require("oil").setup({
 	},
 })
 
-require("mason").setup({})
-require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "pyright", "intelephense", "vimls", "html", "clangd", "jdtls" },
-})
-
-local cmp = require("cmp")
-require("nvim-autopairs").setup({
-	fast_wrap = {},
+require("csvview").setup({
+	view = {
+		display_mode = "border",
+	},
 })
 require("mini.indentscope").setup({
 	draw = {
 		delay = 0,
-		animation = nil,
 	},
 	symbol = "│",
 	options = {
 		try_as_border = true,
 	},
 })
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls", "pyright", "intelephense", "vimls", "html", "clangd", "jdtls" },
+})
+require("mason-tool-installer").setup({
+	ensure_installed = {
+		"ansiblels",
+		"bashls",
+		"black",
+		"clang-format",
+		"clangd",
+		"cssls",
+		"debugpy",
+		"html",
+		"google-java-format",
+		"intelephense",
+		"isort",
+		"lua_ls",
+		"omnisharp",
+		"php-cs-fixer",
+		"prettier",
+		"pyright",
+		"shfmt",
+		"stylua",
+		"vimls",
+		"yamlls",
+		"yamllint",
+	},
+})
+
+local cmp = require("cmp")
+require("nvim-autopairs").setup({
+	fast_wrap = {},
+})
 
 local luasnip = require("luasnip")
-luasnip.filetype_extend("cs", { "csharp" })
-luasnip.filetype_extend("cs", { "unity" })
-require("luasnip.loaders.from_vscode").lazy_load({})
--- require("luasnip.loaders.from_vscode").lazy_load({
--- 	paths = { vim.fn.expand("~/.config/nvim/snippets") },
--- })
+require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
 	snippet = {
@@ -251,25 +233,8 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = false }),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+		["<tab>"] = cmp.mapping.select_next_item(),
+		["<S-tab>"] = cmp.mapping.select_prev_item(),
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
@@ -297,81 +262,65 @@ require("conform").setup({
 	},
 })
 
-local function jdtls_setup()
-	local ok, jdtls = pcall(require, "jdtls")
-	if not ok then
+local function pack_clean()
+	if not (vim.pack and vim.pack.get and vim.pack.del) then
+		vim.notify("vim.pack is not available.", vim.log.levels.WARN)
 		return
 	end
 
-	local mason_path = vim.fn.stdpath("data") .. "/mason"
-	local jdtls_path = mason_path .. "/packages/jdtls"
-	local launcher = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
-
-	-- CONFIG por SO (Mac/Linux/Windows)
-	local config_os
-	if vim.fn.has("mac") == 1 then
-		config_os = jdtls_path .. "/config_mac"
-	elseif vim.fn.has("unix") == 1 then
-		config_os = jdtls_path .. "/config_linux"
-	else
-		config_os = jdtls_path .. "/config_win"
+	local stale_specs = {}
+	for _, plugin in ipairs(vim.pack.get()) do
+		if not plugin.active then
+			local spec = plugin.spec or {}
+			if type(spec.src) == "string" and spec.src ~= "" then
+				table.insert(stale_specs, { src = spec.src })
+			elseif type(spec.name) == "string" and spec.name ~= "" then
+				table.insert(stale_specs, { name = spec.name })
+			end
+		end
 	end
 
-	-- workspace por projeto
-	local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-	local workspace_dir = vim.fn.stdpath("data") .. "/jdtls-workspace/" .. project_name
+	if #stale_specs == 0 then
+		vim.notify("No unused plugins to clean.", vim.log.levels.INFO)
+		return
+	end
 
-	local config = {
-		cmd = {
-			"java",
-			"-Declipse.application=org.eclipse.jdt.ls.core.id1",
-			"-Dosgi.bundles.defaultStartLevel=4",
-			"-Declipse.product=org.eclipse.jdt.ls.core.product",
-			"-Dlog.protocol=true",
-			"-Dlog.level=ALL",
-			"-Xms1g",
-			"--add-modules=ALL-SYSTEM",
-			"--add-opens",
-			"java.base/java.util=ALL-UNNAMED",
-			"--add-opens",
-			"java.base/java.lang=ALL-UNNAMED",
-			"-jar",
-			launcher,
-			"-configuration",
-			config_os,
-			"-data",
-			workspace_dir,
-		},
-
-		root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
-
-		settings = {
-			java = {
-				signatureHelp = { enabled = true },
-				contentProvider = { preferred = "fernflower" },
-				completion = {
-					favoriteStaticMembers = {
-						"org.junit.jupiter.api.Assertions.*",
-						"java.util.Objects.requireNonNull",
-						"java.util.Objects.requireNonNullElse",
-					},
-				},
-			},
-		},
-
-		capabilities = require("cmp_nvim_lsp").default_capabilities(),
-	}
-
-	jdtls.start_or_attach(config)
+	local choice = vim.fn.confirm(("Remove %d unused plugin(s)?"):format(#stale_specs), "&Yes\n&No", 2)
+	if choice == 1 then
+		local removed = 0
+		local failed = 0
+		for _, spec in ipairs(stale_specs) do
+			local ok = pcall(vim.pack.del, { spec })
+			if ok then
+				removed = removed + 1
+			else
+				failed = failed + 1
+			end
+		end
+		if failed == 0 then
+			vim.notify(("Removed %d unused plugin(s)."):format(removed), vim.log.levels.INFO)
+		else
+			vim.notify(
+				("Removed %d plugin(s), failed to remove %d."):format(removed, failed),
+				vim.log.levels.WARN
+			)
+		end
+	end
 end
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "java",
-	callback = jdtls_setup,
-})
-
 vim.cmd("colorscheme rose-pine")
-vim.cmd("hi statusline guibg=#26233a guifg=#e0def4")
+
+vim.api.nvim_set_hl(0, "StatusLine", {
+	bg = "#1C1C39",
+	fg = "#81839E",
+})
+vim.api.nvim_set_hl(0, "StatusLineNC", {
+	bg = "#141428",
+	fg = "#4A4C6A",
+})
+vim.api.nvim_set_hl(0, "ColorColumn", {
+	bg = "#1C1C39",
+})
 require("colorizer").setup({})
 
 local ok_builtin, builtin = pcall(require, "telescope.builtin")
@@ -380,13 +329,10 @@ if ok_builtin then
 	vim.keymap.set("n", "<leader>fw", builtin.live_grep,  { desc = "Live grep" })
 	vim.keymap.set("n", "<leader>h",  builtin.help_tags,  { desc = "Help tags" })
 end
-if ok_99 then
-	vim.keymap.set("v", "<leader>9v", function() _99.visual() end,            { desc = "99: Replace visual with AI" })
-	vim.keymap.set("n", "<leader>9s", function() _99.search() end,            { desc = "99: Search project with AI" })
-	vim.keymap.set("n", "<leader>9o", function() _99.open() end,              { desc = "99: Open last interaction" })
-	vim.keymap.set("n", "<leader>9x", function() _99.stop_all_requests() end, { desc = "99: Stop requests" })
-	vim.keymap.set("n", "<leader>9l", function() _99.view_logs() end,         { desc = "99: View request logs" })
-end
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "<leader>pc", pack_clean)
 vim.keymap.set("n", "<leader>o", ":Oil<CR>")
 vim.keymap.set("n", "<C-c>", ":%y+<CR>")
 vim.keymap.set("n", "<leader>lf", function()
@@ -400,6 +346,7 @@ vim.keymap.set("n", "<leader>sh", "<C-w>s")
 vim.keymap.set("n", "<leader>se", "<C-w>=")
 vim.keymap.set("n", "<leader>sr", ":AutoSession restore<CR>")
 vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>")
+vim.keymap.set("n", "<leader>gh", ":GBrowse<CR>", { desc = "Open on GitHub", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>vrn", function()
 	vim.lsp.buf.rename()
 end, opts)
